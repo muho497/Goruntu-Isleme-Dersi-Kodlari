deaa714fb07a4db214527e68ab23a720f9466069
muho497 <114869487+muho497@users.noreply.github.com>
2022-10-26 23:07:29 +0300

    Update HistogramHesaplama.py

--- Histogram/HistogramHesaplama.py
+++ Histogram/HistogramHesaplama.py
@@ -1,23 +1,28 @@
-from numpy import zeros, shape
-from matplotlib import pyplot as plt
-import cv2
 import numpy as np
-
-deneme = "deneme.jpg"
-resim = cv2.imread(deneme)
+import cv2
 
 
+img=cv2.imread("deneme.jpg",0)
 
-h = resim.shape[0]
-w = resim.shape[1]
 
-Hist = zeros(256);
-[w,h] = shape(I);
-for v in range (0, u<h):
-    for u in range (0, u<w):
-        i= I(u,v)
-        Hist[i]=Hist[i]+1
+#cv2.imshow('image',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 
+
+a=np.zeros((256,),dtype=np.float16)
+
+height,width=img.shape
+
+
+for i in range(width):
+    for j in range(height):
+                  g=img[j,i]
+                  a[g]=a[g]+1
+print(a)
+#hist = cv2.calcHist([img],[0],None,[256],[0,256])
+#cv2.imshow('hist',hist)
+#cv2.waitKey(0)
 
 
 
